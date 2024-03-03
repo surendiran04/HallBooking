16fbcf48d0d4a911a2ca46cd9262720b0a773625
surendiran04 <surendiran.m2004@gmail.com>
2024-03-03 22:07:37 +0530

    Finished Api's

--- src/Routes/Rooms.js
+++ src/Routes/Rooms.js
@@ -1,8 +1,12 @@
 const express=require('express');
-const { createRoom } = require("../Contoller/Roomcontoller.js")
+const { createRoom,BookRoom,getRooms,getBookedrooms,getCustomersbookings,getBookingCount } = require("../Contoller/Roomcontoller.js")
 const router = express.Router();
 
 router.post("/createRoom",createRoom)
-
+router.post("/bookRoom",BookRoom)
+router.get("/",getRooms)
+router.get("/bookedrooms",getBookedrooms)
+router.get("/customers",getCustomersbookings)
+router.get("/bookingcount/:CustomerName",getBookingCount)
 
 module.exports = router;
