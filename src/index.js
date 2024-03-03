ee51fe984b1c9946b20d1a595adc497fe8178fd2
surendiran04 <surendiran.m2004@gmail.com>
2024-03-03 23:32:55 +0530

    added ENV variables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = require('./Routes/Rooms.js')  
 const app=express();
-const PORT=8000;
+require("dotenv").config();
+
+const PORT=process.env.PORT
 
 app.use(express.json());
-
 
 
 app.use(router);
